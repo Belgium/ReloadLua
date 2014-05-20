d8f55bc5a966a2515e554ebcbe72da4b1def25bc
TopNotch <topnotch@mail.be>
2014-05-20 17:22:53 +0200

    Fixed, but it still is simple as fuck lol

--- Reload.lua
+++ Reload.lua
@@ -6,13 +6,12 @@ This script is not complete yet since it would lead to a crash (last tested with
 addhook("say","_say")
 function _say(id,txt)
 	if txt=="!reset" then
-		msg2(id,"Updating server to the current script...")
+		msg2(id,"Updating server to the latest saved server.lua file...")
 		resetscript()
 	end
 end
 
 function resetscript()
-	parse("luareset")
 	dofile("sys/lua/server.lua")
-	msg2(id,"The server is up to date with the current script.")
+	msg2(id,"The server is up to date with the latest script version.")
 end
